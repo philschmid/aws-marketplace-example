7d1d31cb7f18531b85d6402e2525bb78bc25f80e
philschmid <schmidphilipp1995@gmail.com>
2023-02-23 21:49:43 +0100

    working example e2e

--- api/track_usage.py
+++ api/track_usage.py
@@ -5,23 +5,40 @@ import os
 
 PRODUCT_CODE = os.environ.get("PRODUCT_CODE", None)
 
-
-def handler(event):
+def handler(event, context=None):
     print(event)
     # loads the incoming event into a dictonary
     body = json.loads(event["body"])
-
+    marketplaceClient = boto3.client("meteringmarketplace")
     usageRecord = [
         {
             "Timestamp": int(time.time()),
-            "CustomerIdentifier": body["customerID"],
+            "CustomerIdentifier": body["CustomerIdentifier"],
             "Dimension": "cpu_count",
             "Quantity": 1,
         }
     ]
 
-    marketplaceClient = boto3.client("meteringmarketplace")
-
+    # send usage record to AWS Marketplace
     response = marketplaceClient.batch_meter_usage(UsageRecords=usageRecord, ProductCode=PRODUCT_CODE)
+    
+    # check if any Status any UsageRecord is Successs
+    if any(record["Status"] == "Success" for record in response["Results"]):
+        return  {
+            "statusCode": 200,
+            "headers": {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Credentials": True,
+            }
+        }
 
-    return True
+    
+    raise Exception("Failed to meter usage")
+
+
+if __name__ == "__main__":
+    event = {
+        "body": '{"CustomerIdentifier": "gmEGnEoSxoP"}',
+    }
+    print(handler(event))
+    
